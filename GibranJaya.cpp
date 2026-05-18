aca73415e9517de751b06c64453de583eeabe85b
Faizaditya-oss <faizaditya750@gmail.com>
2026-05-18 20:43:53 +0700

    Menambahkan child dari rekeningbank

--- GibranJaya.cpp
+++ GibranJaya.cpp
@@ -35,4 +35,45 @@ public:
     }
 };
 
+class RekeningySyariah : public RekeningBank {
 
+public:
+    RekeningySyariah(string nama, string noRek, double saldoAwal)
+        : RekeningBank(nama, noRek, saldoAwal) {
+    }
+    void potongAdmin() override {
+        cout << "  [INFO] Rekening Syariah - bebas biaya admin!" << endl;
+        cout << "  Saldo tetap: Rp " << saldo << endl;
+    }
+
+    string getJenis() override {
+        return "Rekening Syariah";
+    }
+};
+
+class RekeningKonvensional : public RekeningBank {
+
+private:
+    static const double BIAYA_ADMIN; 
+
+public:
+    RekeningKonvensional(string nama, string noRek, double saldoAwal)
+        : RekeningBank(nama, noRek, saldoAwal) {}
+
+    void potongAdmin() override {
+        if (saldo >= BIAYA_ADMIN) {
+            saldo = saldo - BIAYA_ADMIN; 
+            cout << "  [POTONG] Biaya admin Rp " << BIAYA_ADMIN << " berhasil dipotong." << endl;
+        } else {
+            cout << "  [GAGAL] Saldo kurang! Minimal harus ada Rp " << BIAYA_ADMIN << endl;
+        }
+        cout << "  Saldo sekarang: Rp " << saldo << endl;
+    }
+
+    string getJenis() override {
+        return "Rekening Konvensional";
+    }
+};
+
+// Nilai static diluar class
+const double RekeningKonvensional::BIAYA_ADMIN = 15000.0;
